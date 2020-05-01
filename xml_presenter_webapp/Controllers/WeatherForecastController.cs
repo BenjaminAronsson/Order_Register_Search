abbd32a647529443916f58ba51dcf1d20922bcf6
Benjamin <bene.a99@gmail.com>
2020-05-01 21:39:06 +0200

    merge orders and remove headers

--- xml_presenter_webapp/Controllers/WeatherForecastController.cs
+++ xml_presenter_webapp/Controllers/WeatherForecastController.cs
@@ -28,59 +28,41 @@ namespace xml_presenter_webapp.Controllers
         [HttpGet]
         public IEnumerable<WeatherForecast> Get()
         {
-            System.Console.WriteLine("hello there!");
+            //find files
+            string[] txtfiles = Directory.GetFiles("db", "*.txt");
+            
+            // Read into an array of strings.  
+            List<string> source = new List<string>();
+            
+            foreach(string file in txtfiles) { 
+                //read file from disk
+                var linesFromFile = System.IO.File.ReadAllLines(file);
+                
+                //remove table headers
+                linesFromFile = linesFromFile.Skip(1).ToArray();
+                //combine files
+                source.AddRange(linesFromFile);  
+            }
+            string[] modifiedSource = source.Skip(8).ToArray();
+            XElement cust = new System.Xml.Linq.XElement("Orders",  
+                from str in source  
+                let fields = str.Split('|')  
+                select new XElement("Order",  
+                    new XAttribute("OrderNumber", fields[1]),  
+                    new XElement("OrderLineNumber", fields[2]),  
+                    new XElement("ProductNumber", fields[3]),  
+                    new XElement("Quantity", fields[4]),  
+                    new XElement("Name", fields[5]),  
+                    new XElement("Description", fields[6]),  
+                    new XElement("Price", fields[7]),  
+                    new XElement("ProductGroup", fields[8]),  
+                    new XElement("OrderDate", fields[9]),  
+                    new XElement("CustomerName", fields[10]),  
+                    new XElement("CustomerNumber", fields[11])
+                )  
+            );
 
-
-//FileInfo[] as = dinfo.GetFiles("*.txt"); 
-string[] txtfiles = Directory.GetFiles("db", "*.txt");
-  
-// Read into an array of strings.  
-List<string> source = new List<string>();
- 
-foreach(string file in txtfiles) { 
-    source.AddRange(System.IO.File.ReadAllLines(file));  
-}
-string[] modifiedSource = source.Skip(8).ToArray();
-XElement cust = new System.Xml.Linq.XElement("Orders",  
-    from str in source  
-    let fields = str.Split('|')  
-    select new XElement("Order",  
-        new XAttribute("OrderNumber", fields[1]),  
-        new XElement("OrderLineNumber", fields[2]),  
-        new XElement("ProductNumber", fields[3]),  
-        new XElement("Quantity", fields[4]),  
-        new XElement("Name", fields[5]),  
-        new XElement("Description", fields[6]),  
-        new XElement("Price", fields[7]),  
-        new XElement("ProductGroup", fields[8]),  
-        new XElement("OrderDate", fields[9]),  
-        new XElement("CustomerName", fields[10]),  
-        new XElement("CustomerNumber", fields[11])
-    )  
-);
-// {
-// foreach(string n in source) {
-
-//     Console.WriteLine(n);
-// }}
-
-//remove rows
-cust.Elements().ToArray()[0].Remove();
-
-System.Console.WriteLine(cust);
-
-
-
-
-
-            var rng = new Random();
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateTime.Now.AddDays(index),
-                TemperatureC = rng.Next(-20, 55),
-                Summary = Summaries[rng.Next(Summaries.Length)]
-            })
-            .ToArray();
+            return cust;
         }
     }
 }
