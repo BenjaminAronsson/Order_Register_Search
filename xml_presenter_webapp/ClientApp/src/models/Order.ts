c70adbdf8acc033408e4e8fee2cf23a9f468b6b8
Benjamin <bene.a99@gmail.com>
2020-05-02 18:51:58 +0200

    displaying order information

--- xml_presenter_webapp/ClientApp/src/models/Order.ts
+++ xml_presenter_webapp/ClientApp/src/models/Order.ts
@@ -3,13 +3,17 @@ export class Order {
     date: string;
     OrderNumber: string;
     OrderLineNumber: string;
+    products: Product[];
+    OrderDate: string;
+    CustomerName: string;
+    CustomerNumber: string;
+  }
+
+  export class Product {
     ProductNumber: string;
     Quantity: string;
     Name: string;
     Description: string;
     Price: string;
     ProductGroup: string;
-    OrderDate: string;
-    CustomerName: string;
-    CustomerNumber: string;
   }
